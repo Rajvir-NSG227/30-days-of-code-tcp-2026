c7dd385ad23bcd87ef29cfe5c1a0e7e5a0985c69
RAJVIR SHAW <rajvirshawnitrr@gmail.com>
2026-01-13 22:37:55 +0530

    corrections

--- day_13/day_13.py
+++ day_13/day_13.py
@@ -3,10 +3,11 @@ def minEatingSpeed(self, piles: list[int], h: int) -> int:
     k_min = (sum(piles)+h-1) // h # gives upper value of division
     k_max = max(piles)
     l,r = k_min,k_max
-    k = (l+r)//2
-    t = sum((p+k-1//k) for p in piles)
-    if (t<=h):
-        r = k
-    else: # t exceeding h
-        l=k+1
+    while l<r:
+        k = (l+r)//2
+        t = sum((p+k-1//k) for p in piles)
+        if (t<=h):
+            r = k
+        else: # t exceeding h
+            l=k+1
     return l
